47c1bb23aeb04da1237d1e96aff3cf11e9428487
quinnhyx <qhe@smith.edu>
2026-03-13 16:47:35 -0400

    add trc for fixed x0

--- plot.py
+++ plot.py
@@ -9,7 +9,6 @@ from matplotlib.animation import FuncAnimation
 # -----------------------------
 files = sorted(glob.glob("snapshots/snapshots_s*.h5"))
 
-# p = []
 trC = []
 ux = []
 uy = []
@@ -18,7 +17,6 @@ times = []
 for fname in files:
     with h5py.File(fname, "r") as f:
 
-        # p.append(f["tasks"]["p"][:])
         ux.append(f["tasks"]["ux"][:])
         uy.append(f["tasks"]["uy"][:])
 
@@ -28,7 +26,6 @@ for fname in files:
 
         times.append(f["scales"]["sim_time"][:])
 
-# p = np.concatenate(p)
 ux = np.concatenate(ux)
 uy = np.concatenate(uy)
 trC = np.concatenate(trC)
@@ -37,33 +34,48 @@ times = np.concatenate(times)
 u_mag = np.sqrt(ux**2 + uy**2)
 
 print("Total frames:", len(times))
+print("Data shape:", trC.shape)
 
 # -----------------------------
-# Color limits (avoid flicker)
+# Grid info
 # -----------------------------
-# pmin, pmax = p.min(), p.max()
+Nt, Nx, Ny = trC.shape
+
+x0_index = Nx // 2   # 固定 x0 在中间
+y = np.arange(Ny)
+
+# -----------------------------
+# Color limits
+# -----------------------------
 cmin, cmax = trC.min(), trC.max()
 umin, umax = u_mag.min(), u_mag.max()
 
 # -----------------------------
 # Plot setup
 # -----------------------------
-fig, axes = plt.subplots(1,2, figsize=(15,4))
+fig, axes = plt.subplots(1,3, figsize=(18,4))
 
-ax2, ax3 = axes
+ax1, ax2, ax3 = axes
 
-# im1 = ax1.imshow(p[0].T, origin="lower", cmap="coolwarm", vmin=pmin, vmax=pmax)
-# ax1.set_title("Pressure p")
+# trC field
+im1 = ax1.imshow(trC[0].T, origin="lower",
+                 cmap="viridis", vmin=cmin, vmax=cmax)
+ax1.set_title("Polymer Stretch tr(C)")
 
-im2 = ax2.imshow(trC[0].T, origin="lower", cmap="viridis", vmin=cmin, vmax=cmax)
-ax2.set_title("Polymer Stretch tr(C)")
+# velocity magnitude
+im2 = ax2.imshow(u_mag[0].T, origin="lower",
+                 cmap="plasma", vmin=umin, vmax=umax)
+ax2.set_title("Velocity |u|")
 
-im3 = ax3.imshow(u_mag[0].T, origin="lower", cmap="plasma", vmin=umin, vmax=umax)
-ax3.set_title("Velocity |u|")
+# slice plot
+line, = ax3.plot(y, trC[0, x0_index, :], linewidth=2)
+ax3.set_ylim(cmin, cmax)
+ax3.set_title(f"tr(C) slice at x index = {x0_index}")
+ax3.set_xlabel("y")
+ax3.set_ylabel("tr(C)")
 
-# plt.colorbar(im1, ax=ax1)
+plt.colorbar(im1, ax=ax1)
 plt.colorbar(im2, ax=ax2)
-plt.colorbar(im3, ax=ax3)
 
 title = fig.suptitle("")
 
@@ -72,18 +84,19 @@ title = fig.suptitle("")
 # -----------------------------
 def update(i):
 
-    # im1.set_data(p[i].T)
-    im2.set_data(trC[i].T)
-    im3.set_data(u_mag[i].T)
+    im1.set_data(trC[i].T)
+    im2.set_data(u_mag[i].T)
+
+    line.set_ydata(trC[i, x0_index, :])
 
     title.set_text(f"t = {times[i]:.3f}")
 
-    return im2, im3
+    return im1, im2, line
 
 ani = FuncAnimation(fig, update, frames=len(times), interval=60)
 
 plt.tight_layout()
 plt.show()
 
-# save if needed
-# ani.save("three_panel_flow.mp4", dpi=200)
+# 保存动画
+# ani.save("flow_slice_animation.mp4", dpi=200)
